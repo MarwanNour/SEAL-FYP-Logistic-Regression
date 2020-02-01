badf26ef31479b2f499e49f95fb8bf578992ffcc
Marwan Nour <marwan.nr64@gmail.com>
2020-02-02 00:43:26 +0200

    Updated ckks

--- 4_ckks.cpp
+++ 4_ckks.cpp
@@ -1,13 +1,99 @@
 #include "seal/seal.h"
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 using namespace seal;
 
+// Helper function that prints a vector of floats
+template <typename T>
+inline void print_vector(std::vector<T> vec, std::size_t print_size = 4, int prec = 3)
+{
+    /*
+    Save the formatting information for std::cout.
+    */
+    std::ios old_fmt(nullptr);
+    old_fmt.copyfmt(std::cout);
+
+    std::size_t slot_count = vec.size();
+
+    std::cout << std::fixed << std::setprecision(prec);
+    std::cout << std::endl;
+    if (slot_count <= 2 * print_size)
+    {
+        std::cout << "    [";
+        for (std::size_t i = 0; i < slot_count; i++)
+        {
+            std::cout << " " << vec[i] << ((i != slot_count - 1) ? "," : " ]\n");
+        }
+    }
+    else
+    {
+        vec.resize(std::max(vec.size(), 2 * print_size));
+        std::cout << "    [";
+        for (std::size_t i = 0; i < print_size; i++)
+        {
+            std::cout << " " << vec[i] << ",";
+        }
+        if (vec.size() > 2 * print_size)
+        {
+            std::cout << " ...,";
+        }
+        for (std::size_t i = slot_count - print_size; i < slot_count; i++)
+        {
+            std::cout << " " << vec[i] << ((i != slot_count - 1) ? "," : " ]\n");
+        }
+    }
+    std::cout << std::endl;
+
+    /*
+    Restore the old std::cout formatting.
+    */
+    std::cout.copyfmt(old_fmt);
+}
+
 int main()
 {
-    cout <<"------- CKKS ---------\n" << endl;
+    cout << "------- CKKS ---------\n"
+         << endl;
 
+    // Set up the parameters
+    EncryptionParameters params(scheme_type::CKKS);
+    size_t poly_modulus_degree = 8192;
+    params.set_poly_modulus_degree(poly_modulus_degree);
+    params.set_coeff_modulus(CoeffModulus::Create(poly_modulus_degree, {60, 40, 40, 60}));
+
+    double scale = pow(2.0, 40);
+    auto context = SEALContext::Create(params);
+
+    // Generate keys, encryptor, decryptor and evaluator
+    KeyGenerator keygen(context);
+    PublicKey pk = keygen.public_key();
+    SecretKey sk = keygen.secret_key();
+    RelinKeys relin_keys = keygen.relin_keys();
+
+    Encryptor encryptor(context, pk);
+    Decryptor decryptor(context, sk);
+    Evaluator evaluator(context);
+
+    CKKSEncoder encoder(context);
+    size_t slot_count = encoder.slot_count();
+    cout << "Number of slots: " << slot_count << endl;
+
+    vector<double> input_vec;
+    input_vec.reserve(slot_count);
+
+    double curr_point = 0;
+    double step_size = 1.0 / (static_cast<double>(slot_count) - 1);
+    for (size_t i = 0; i < slot_count; i++, curr_point += step_size)
+    {
+        input_vec.push_back(curr_point);
+    }
+
+    cout << "Input Vector: " << endl;
+    print_vector(input_vec, 3, 7);
+
+    cout << "Evaluating polynomial PI*x^3 + 0.4x + 1 ..." << endl;
 
     return 0;
 }
