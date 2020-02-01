4992d999fc2628c423a7b9e5afb10f63ba903591
Marwan Nour <marwan.nr64@gmail.com>
2020-02-01 11:27:31 +0200

    Added Integer Encoding

--- 1_bfv.cpp
+++ 1_bfv.cpp
@@ -37,14 +37,14 @@ int main()
     as plaintext inputs, as we will see below. The computation is done modulo the
     plain_modulus 1024.
 
-    To get started, we create a plaintext containing the constant 6. For the
+    To get started, we create a plaintext containing the constant 10. For the
     plaintext element we use a constructor that takes the desired polynomial as
     a string with coefficients represented as hexadecimal numbers.
     */
-	int x = 6;
+	int x = 10;
 	Plaintext x_plain(to_string(x));
 	cout << "Express x = " + to_string(x) +
-				" as a plaintext polynomial 0x" + x_plain.to_string() + "."
+				" as a plaintext polynomial " + x_plain.to_string() + "."
 		 << endl;
 
 	/* We then encrypt the plaintext, producing a ciphertext. */
@@ -54,8 +54,7 @@ int main()
 
 	/* In Microsoft SEAL, a valid ciphertext consists of two or more polynomials
     whose coefficients are integers modulo the product of the primes in the
-    coeff_modulus. The number of polynomials in a ciphertext is called its `size'
-    and is given by Ciphertext::size(). A freshly encrypted ciphertext always
+    coeff_modulus. A freshly encrypted ciphertext always
     has size 2. */
 	cout << "    + size of freshly encrypted x: " << x_encrypted.size() << endl;
 
@@ -68,7 +67,7 @@ int main()
 	Plaintext x_decrypted;
 	cout << "    + decryption of x_encrypted: ";
 	decryptor.decrypt(x_encrypted, x_decrypted);
-	cout << "0x" << x_decrypted.to_string() << " ...... Correct." << endl;
+	cout << "" << x_decrypted.to_string() << " ...... Correct." << endl;
 
 	/* When using Microsoft SEAL, it is typically advantageous to compute in a way
     that minimizes the longest chain of sequential multiplications. In other
@@ -80,12 +79,7 @@ int main()
         4x^4 + 8x^3 + 8x^2 + 8x + 4 = 4(x + 1)^2 * (x^2 + 1)
 
     to obtain a simple depth 2 representation. Thus, we compute (x + 1)^2 and
-    (x^2 + 1) separately, before multiplying them, and multiplying by 4.
-
-    First, we compute x^2 and add a plaintext "1". We can clearly see from the
-    print-out that multiplication has consumed a lot of noise budget. The user
-    can vary the plain_modulus parameter to see its effect on the rate of noise
-    budget consumption. */
+    (x^2 + 1) separately, before multiplying them, and multiplying by 4. */
 
 	cout << "Compute x_sq_plus_one (x^2+1)." << endl;
 	Ciphertext x_sq_plus_one;
@@ -111,7 +105,7 @@ int main()
 	Plaintext decrypted_result;
 	cout << "    + decryption of x_sq_plus_one: ";
 	decryptor.decrypt(x_sq_plus_one, decrypted_result);
-	cout << "0x" << decrypted_result.to_string() << " ...... Correct." << endl;
+	cout << "" << decrypted_result.to_string() << " ...... Correct." << endl;
 
 	/*
     Next, we compute (x + 1)^2.
@@ -126,7 +120,7 @@ int main()
 		 << " bits" << endl;
 	cout << "    + decryption of x_plus_one_sq: ";
 	decryptor.decrypt(x_plus_one_sq, decrypted_result);
-	cout << "0x" << decrypted_result.to_string() << " ...... Correct." << endl;
+	cout << "" << decrypted_result.to_string() << " ...... Correct." << endl;
 
 	/*
     Finally, we multiply (x^2 + 1) * (x + 1)^2 * 4.
@@ -161,7 +155,7 @@ int main()
 		 << decryptor.invariant_noise_budget(x_sq_plus_one) << " bits" << endl;
 	cout << "    + decryption of x_sq_plus_one: ";
 	decryptor.decrypt(x_sq_plus_one, decrypted_result);
-	cout << "0x" << decrypted_result.to_string() << " ...... Correct." << endl;
+	cout << "" << decrypted_result.to_string() << " ...... Correct." << endl;
 
 	Ciphertext x_plus_one;
 	cout << "Compute x_plus_one (x+1), then compute and relinearize x_plus_one_sq ((x+1)^2)." << endl;
@@ -173,7 +167,7 @@ int main()
 		 << decryptor.invariant_noise_budget(x_plus_one_sq) << " bits" << endl;
 	cout << "    + decryption of x_plus_one_sq: ";
 	decryptor.decrypt(x_plus_one_sq, decrypted_result);
-	cout << "0x" << decrypted_result.to_string() << " ...... Correct." << endl;
+	cout << "" << decrypted_result.to_string() << " ...... Correct." << endl;
 
 	cout << "Compute and relinearize encrypted_result (4(x^2+1)(x+1)^2)." << endl;
 	evaluator.multiply_plain_inplace(x_sq_plus_one, plain_four);
@@ -192,11 +186,11 @@ int main()
     of noise budget left, so we can expect the correct answer when decrypting. */
 	cout << "Decrypt encrypted_result (4(x^2+1)(x+1)^2)." << endl;
 	decryptor.decrypt(encrypted_result, decrypted_result);
-	cout << "    + decryption of 4(x^2+1)(x+1)^2 = 0x"
+	cout << "    + decryption of 4(x^2+1)(x+1)^2 = "
 		 << decrypted_result.to_string() << " ...... Correct." << endl;
 	cout << endl;
 
 	/*  For x=6, 4(x^2+1)(x+1)^2 = 7252. Since the plaintext modulus is set to 1024,
     this result is computed in integers modulo 1024. Therefore the expected output
-    should be 7252 % 1024 == 84, or 0x54 in hexadecimal. */
+    should be 7252 % 1024 == 84, or 54 in hexadecimal. */
 }
---
+++ 2_encoders.cpp
@@ -0,0 +1,13 @@
+#include "seal/seal.h"
+#include <iostream>
+
+using namespace std;
+using namespace seal;
+
+
+int main()
+{
+    cout << "--------- Integer Encoding ---------" << endl;
+
+    return 0;
+}
