45edceaf8d4d95d467cd86ef0a49487f26998220
Marwan Nour <marwan.nr64@gmail.com>
2020-02-01 19:20:50 +0200

    Updated levels

--- 3_levels.cpp
+++ 3_levels.cpp
@@ -21,5 +21,131 @@ int main()
 
     auto context = SEALContext::Create(params);
 
+    cout << "Print the modulus switching chain" << endl;
+
+    // Print the key level parameter info
+    auto context_data = context->key_context_data();
+    cout << "\tLevel (chain index): " << context_data->chain_index() << endl;
+    // cout << "\tparms_id: " << context_data->parms_id() << endl;
+    cout << "\tcoeff_modulus primes: ";
+    cout << hex;
+    for (const auto &prime : context_data->parms().coeff_modulus())
+    {
+        cout << prime.value() << " ";
+    }
+    cout << dec << endl;
+    cout << "\\" << endl;
+    cout << " \\-->";
+
+    // Iterate over the remaining levels
+    context_data = context->first_context_data();
+    while (context_data)
+    {
+        cout << " Level (chain index): " << context_data->chain_index();
+        if (context_data->parms_id() == context->first_parms_id())
+        {
+            cout << " ...... first_context_data()" << endl;
+        }
+        else if (context_data->parms_id() == context->last_parms_id())
+        {
+            cout << " ...... last_context_data()" << endl;
+        }
+        else
+        {
+            cout << endl;
+        }
+        // cout << "      parms_id: " << context_data->parms_id() << endl;
+        cout << "      coeff_modulus primes: ";
+        cout << hex;
+        for (const auto &prime : context_data->parms().coeff_modulus())
+        {
+            cout << prime.value() << " ";
+        }
+        cout << dec << endl;
+        cout << "\\" << endl;
+        cout << " \\-->";
+
+        /*
+        Step forward in the chain.
+        */
+        context_data = context_data->next_context_data();
+    }
+
+    cout << "End of chain reached\n"
+         << endl;
+
+    // Generate keys
+    KeyGenerator keygen(context);
+    PublicKey pk = keygen.public_key();
+    SecretKey sk = keygen.secret_key();
+    RelinKeys relin_keys = keygen.relin_keys();
+    GaloisKeys gal_keys = keygen.galois_keys();
+
+    Encryptor encryptor(context, pk);
+    Decryptor decryptor(context, sk);
+    Evaluator evaluator(context);
+
+    Plaintext plain("1x^3 + 2x^2 + 3x^1 + 4");
+    Ciphertext cipher;
+    encryptor.encrypt(plain, cipher);
+    cout << "Perform modulus switching on cipher" << endl;
+    context_data = context->first_context_data();
+    cout << "---->";
+
+    while (context_data->next_context_data())
+    {
+        cout << " Level (chain index): " << context_data->chain_index() << endl;
+        // cout << "      parms_id of encrypted: " << cipher.parms_id() << endl;
+        cout << "      Noise budget at this level: "
+             << decryptor.invariant_noise_budget(cipher) << " bits" << endl;
+        cout << "\\" << endl;
+        cout << " \\-->";
+        evaluator.mod_switch_to_next_inplace(cipher);
+        context_data = context_data->next_context_data();
+    }
+    cout << " Level (chain index): " << context_data->chain_index() << endl;
+    // cout << "      parms_id of encrypted: " << cipher.parms_id() << endl;
+    cout << "      Noise budget at this level: "
+         << decryptor.invariant_noise_budget(cipher) << " bits" << endl;
+    cout << "\\" << endl;
+    cout << " \\-->";
+    cout << " End of chain reached" << endl
+         << endl;
+
+    cout << "Decrypt still works after modulus switching." << endl;
+    decryptor.decrypt(cipher, plain);
+    cout << "    + Decryption of encrypted: " << plain.to_string() << endl;
+
+    cout << "\tComputation is more efficient with modulus switching." << endl;
+    cout << "Compute the 8th power." << endl;
+    encryptor.encrypt(plain, cipher);
+    cout << "    + Noise budget fresh:                   "
+         << decryptor.invariant_noise_budget(cipher) << " bits" << endl;
+    evaluator.square_inplace(cipher);
+    evaluator.relinearize_inplace(cipher, relin_keys);
+    cout << "    + Noise budget of the 2nd power:         "
+         << decryptor.invariant_noise_budget(cipher) << " bits" << endl;
+    evaluator.square_inplace(cipher);
+    evaluator.relinearize_inplace(cipher, relin_keys);
+    cout << "    + Noise budget of the 4th power:         "
+         << decryptor.invariant_noise_budget(cipher) << " bits" << endl;
+
+    evaluator.mod_switch_to_next_inplace(cipher);
+    cout << "    + Noise budget after modulus switching:  "
+         << decryptor.invariant_noise_budget(cipher) << " bits" << endl;
+
+    evaluator.square_inplace(cipher);
+    evaluator.relinearize_inplace(cipher, relin_keys);
+    cout << "    + Noise budget of the 8th power:         "
+         << decryptor.invariant_noise_budget(cipher) << " bits" << endl;
+    evaluator.mod_switch_to_next_inplace(cipher);
+    cout << "    + Noise budget after modulus switching:  "
+         << decryptor.invariant_noise_budget(cipher) << " bits" << endl;
+
+    decryptor.decrypt(cipher, plain);
+    cout << "    + Decryption of the 8th power (hexadecimal) ...... Correct." << endl;
+    cout << "    " << plain.to_string() << endl
+         << endl;
+
     return 0;
 }
